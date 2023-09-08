938111eb2babed6e4d55b9abdd80e733e1d5e137
Vlad-Petruk <vladpetruk.00@gmail.com>
2023-09-08 19:04:28 +0300

    changed button's listeners, add clear button

--- main.js
+++ main.js
@@ -3,57 +3,108 @@ const minus = '-';
 const asterics = '*';
 const slash = '/';
 
+let a = '';
+let b = '';
+let operator = '';
+let total = '';
 
-function calculateExpression(expression,operator) {
-    const parts = expression.split(operator);
-    if (parts.length === 2) {
-      const num1 = parseFloat(parts[0]);
-      const num2 = parseFloat(parts[1]);
-      if (!isNaN(num1) && !isNaN(num2)) {
-        if(operator === plus) {
-        return num1 + num2;}
-        else if (operator === minus) {
-            return num1 - num2;
-        } else if (operator === asterics) {
-            return num1 * num2;}
-            else if (operator === slash) {
-                return num1 / num2;}
-      }
-    }
-    return NaN; // Return NaN if the expression is invalid
-  }
+let butCont = document.querySelectorAll('.but-cont');
+let btnOperator = document.querySelectorAll('.btn-operator')
+let display = document.querySelector('#display');
+let btnClear = document.querySelector('.clear');
+let btnEqual = document.querySelector('.equal');
+
+
+// function calculateExpression(expression,operator) {
+//     const parts = expression.split(operator);
+//     if (parts.length === 2) {
+//       const num1 = parseFloat(parts[0]);
+//       const num2 = parseFloat(parts[1]);
+//       if (!isNaN(num1) && !isNaN(num2)) {
+//         if(operator === plus) {
+//         return num1 + num2;}
+//         else if (operator === minus) {
+//             return num1 - num2;
+//         } else if (operator === asterics) {
+//             return num1 * num2;}
+//             else if (operator === slash) {
+//                 return num1 / num2;}
+//       }
+//     }
+//     return NaN; // Return NaN if the expression is invalid
+//   }
 
 function add (a,b) {
-    let addedNumbers = `${a} ${plus} ${b}`;
-    return calculateExpression(addedNumbers,plus);
+    let addedNumbers;
+    a = Number(a);
+    b = Number(b);
+    addedNumbers = a + b;
+    return addedNumbers;
 };
 
 function substract (a,b) {
-	let substractedNumbers = `${a} ${minus} ${b}`;
-    return calculateExpression(substractedNumbers,minus);
+	let substractedNumbers = a - b;
+    return substractedNumbers;
 };
 
 function multiply(a,b) {
-    let multipliedNumbers = `${a} ${asterics} ${b}`;
-    return calculateExpression(multipliedNumbers,asterics);
+    let multipliedNumbers = a * b;
+    return multipliedNumbers;
 }
 
 function division (a,b) {
-    let dividedNumbers = `${a} ${slash} ${b}`
-    return calculateExpression(dividedNumbers,slash);
+    let dividedNumbers = a / b;
+    return dividedNumbers;
 }
 
 function operate (a,operator,b) {
-    if (operator === plus){
+    if (operator === '+'){
         return add(a,b)
-    } else if (operator === minus){
+    } else if (operator === '-'){
         return substract(a,b);
-    } else if (operator === asterics){
+    } else if (operator === '*'){
         return multiply(a,b);
-    } else if (operator === slash) {
+    } else if (operator === '/') {
         return division(a,b);
     }
     
 }
 
-//i wonted to create eventListeners for all buttons, that woud be displaying they textContext directly in display, at once
+butCont.forEach((btn) =>{
+    btn.addEventListener('click',()=>{
+        if(operator == '') {
+        a += btn.textContent;
+        display.value = a;
+    } else {
+        b += btn.textContent;
+        display.value = b;
+    }
+    console.log(a);
+    console.log(b);
+    })
+})
+
+btnOperator.forEach((btn) =>{
+    btn.addEventListener('click',()=>{
+        operator = '';
+        b = '';
+        operator += btn.textContent;
+        console.log(operator);
+    })
+})
+btnEqual.addEventListener('click',()=>{
+    if (a !== '') {
+    let result = operate(a,operator,b);
+    console.log(result)
+    display.value = result;
+    operator = '';
+    b = '';
+    a = result;}
+})
+
+btnClear.addEventListener('click',()=>{
+    a = '';
+    b = '';
+    operator = '';
+    display.value = 0;
+})
